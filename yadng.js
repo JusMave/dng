d2120c0ddb87a791a8eb1882fc30a35d785b40c7
Handong Chen <HChen@jmawireless.com>
2026-01-06 16:01:53 -0500

    Update to Manifest V3

--- yadng.js
+++ yadng.js
@@ -61,66 +61,7 @@ var _drop = function(e) {
 	return false;
 };
 
-var _doYadng = function(_yadng) {
-	chrome.tabs.getSelected(null, function(tab) {
-				if (_yadng.isSearch) {
-					_doSearch(_yadng, tab);
-				} else {
-					_doLink(_yadng, tab);
-				}
-			});
-};
-
-var _doSearch = function(_yadng, tab) {
-	chrome.storage.sync.get(['searchEngines'], function(r) {
-				var se = 0;
-				if (_yadng.endX > _yadng.startX) {
-					se = _yadng.endY > _yadng.startY ? 3 : 1;
-				} else {
-					se = _yadng.endY > _yadng.startY ? 2 : 0;
-				}
-				var url;
-				if (-1 == r.searchEngines[se].id) { // user search engines
-					url = r.searchEngines[se].url.replace('%s',
-							_yadng.selection);
-				} else { // build-in
-					url = _build_in_seach_engines[r.searchEngines[se].id].url
-							.replace('%s', _yadng.selection);
-				}
-				chrome.tabs.create({
-							'url' : url,
-							'active' : false,
-							'index' : tab.index + 1
-						});
-			});
-};
-
-var _doLink = function(_yadng, tab) {
-	chrome.storage.sync.get(['indexMode', 'selectedMode'], function(r) {
-				var ti = tab.index;
-				var index = ti + 1;
-				if (r.indexMode == '2') {
-					index = _yadng.endX > _yadng.startX ? ti + 1 : ti;
-				} else {
-					index = r.indexMode == '0' ? ti + 1 : ti;
-				}
-				var selected = false;
-				if (r.selectedMode == '2') {
-					selected = _yadng.endY > _yadng.startY ? false : true;
-				} else {
-					selected = r.selectedMode == '0' ? true : false;
-				}
-				chrome.tabs.create({
-							'url' : _yadng.selection,
-							'active' : selected,
-							'index' : index
-						});
-			});
-};
-
-chrome.runtime.onConnect.addListener(function(port) {
-			port.onMessage.addListener(_doYadng);
-		});
+// Content-only: drag handlers send a message to the background service worker.
 document.addEventListener('dragstart', _dragstart, false);
 document.addEventListener('dragover', _dragover, false);
 document.addEventListener('drop', _drop, false);
